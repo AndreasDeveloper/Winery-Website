fe49d5d1b2d2bd242f0342ae76ba1603d7334059
AndreasDEV <andreasmrdja16@gmail.com>
2019-02-13 22:36:14 +0100

    Optimized website. Added lazy-load to all the images using observer (IntersectionObserver). | Update #4 |

--- resources/js/index.js
+++ resources/js/index.js
@@ -27,3 +27,29 @@ import '../sass/main.scss';
     };
     navBtn.addEventListener('click', addClassFunSix);
 })();
+
+// - IMAGE OBSERVING | OPTIMIZATION - \\
+(() => {
+    // DOM Element
+    const images = document.querySelectorAll('img');
+    // - Function | - For lazy loading images
+    const lazyLoad = target => {
+        const io = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                console.log('Intersecting');
+                // If image is intersecting, run the code and load the image
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    const src = img.getAttribute('data-lazy');
+                    // Setting attribute to src and adding fade class (effect/transition)
+                    img.setAttribute('src', src);
+                    img.classList.add('fade');
+                    // After everything is done, observer will disconnect
+                    observer.disconnect();
+                }
+            });
+        });
+        io.observe(target); // Calling observer on targets
+    };
+images.forEach(lazyLoad); // Lazy load every single img in HTML
+})();
